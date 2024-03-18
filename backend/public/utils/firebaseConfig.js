5a6b622dd10da7202774abebdaf7d651f0cc71f7
umairghani156 <umairghani156@gmail.com>
2024-03-18 14:31:17 +0500

    changesCommited

--- backend/public/utils/firebaseConfig.js
+++ backend/public/utils/firebaseConfig.js
@@ -8,7 +8,7 @@ import { getStorage, ref, uploadBytes,uploadBytesResumable, getDownloadURL } fro
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+////////
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
