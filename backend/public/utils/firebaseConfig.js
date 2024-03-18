55d169d0161cf52b682388da33305d1ce0ce855e
umairghani156 <umairghani156@gmail.com>
2024-03-18 14:28:24 +0500

    changesCommitted

--- backend/public/utils/firebaseConfig.js
+++ backend/public/utils/firebaseConfig.js
@@ -8,15 +8,6 @@ import { getStorage, ref, uploadBytes,uploadBytesResumable, getDownloadURL } fro
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDnjNRjq583w2mqTLEPEgItZkUrHVN7SBA",
-//   authDomain: "first-frontend-project-da025.firebaseapp.com",
-//   projectId: "first-frontend-project-da025",
-//   storageBucket: "first-frontend-project-da025.appspot.com",
-//   messagingSenderId: "796702314001",
-//   appId: "1:796702314001:web:573192cd76c55c62b36bb0",
-//   measurementId: "G-9BVN24D0BP"
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
